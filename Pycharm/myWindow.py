092f4ed5fd22a73c0d46d3da9832132b5f49b9f1
janghee-study <63087915+janghee-study@users.noreply.github.com>
2020-11-10 20:28:46 +0900

    Add files via upload

---
+++ Pycharm/myWindow.py
@@ -0,0 +1,29 @@
+import sys
+import urllib.request
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("pixmapTest.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+        self.qPixmapFileVar = QPixmap()
+        self.qPixmapFileVar.load("target.jpg")
+        self.label.setPixmap(self.qPixmapFileVar)
+        # 버튼에 기능을 연결하는 코드
+        self.start.clicked.connect(self.button1Function)
+
+    # btn_1이 눌리면 작동할 함수
+    def button1Function(self):
+        print("얼굴인식을 시작하겠씁니다")
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
